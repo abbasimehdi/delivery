e7acd15227e1a9c0f6703956ea6d5edce042a9cd
mehdi abbasi <abbasimehdi24@gmail.com>
2023-05-20 15:58:05 +0330

    (refactor): Add base constant

---
+++ app/Constants/BaseConstants.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace torfeh\modules\base\Models\Schemas\Constants;
+
+class BaseConstants
+{
+    const PASSWORD = 'password';
+    const PASSWORD_VALUE = '12345678';
+    const LOGIN = '/login';
+    const AUTH_LOGIN = 'auth.login';
+    const HOME = '/home';
+    const EMAIL = 'email';
+    const ALL_SLUGS = [
+        self::PASSWORD,
+        self::PASSWORD_VALUE,
+        self::HOME,
+        self::LOGIN,
+        self::AUTH_LOGIN,
+        self::EMAIL,
+    ];
+
+}
