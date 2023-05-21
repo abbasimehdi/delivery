f898840b97eb4dff9ed6fbc0852ad1eb393a6339
mehdi abbasi <abbasimehdi24@gmail.com>
2023-05-21 09:44:59 +0330

    (feature-city): Add basically feature test

---
+++ tests/Feature/City/CityTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\City;
+
+use App\Models\City;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CityTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->city = $this->actingAs(City::factory()->create());
+    }
+
+    /** @test */
+    public function authenticated_users_can_create_a_new_city()
+    {
+        //When user submits post request to create city endpoint
+        $this->post('/admin/cities', (array)$this->city);
+
+        //It gets stored in the database
+        $this->assertEquals(1, City::all()->count());
+    }
+
+    /** @test */
+    public function unauthenticated_users_cannot_create_a_new_city()
+    {
+        //Given we have a task object
+        $city = \App\Models\City::factory()->create();
+        //When unauthenticated user submits post request to create task endpoint
+        // He should be redirected to login page
+        $this->post('/cities/store',$city->toArray());
+        $this->get('/');
+    }
+}
